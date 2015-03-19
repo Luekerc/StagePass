7c54c6d5440f757d509b9913d288c02784e6e784
Charles Lueker <charleslueker@yahoo.com>
2015-03-19 16:04:39 -0500

    added first google map code

--- .tmp/public/js/app/controllers.js
+++ .tmp/public/js/app/controllers.js
@@ -89,4 +89,13 @@ angular.module('app.controllers', ['app.services'])
    			})
 		$scope.schotz=false;
 	}
+	// function initialize() {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+      // }
+      // google.maps.event.addDomListener(window, 'load', initialize);
 });
